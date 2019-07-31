054602832f41a65f6a74d28492eb25bcb2be8778
Frank Martinez <frankie.martinez@gmail.com>
2019-07-30 18:12:37 -0700

    Removed Postgres

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,18 @@
 
-const productionDbConnection = process.env.DATABASE_URL || process.env.NODE_PG_DB || "localhost";
-
-// console.log(process.env.NODE_PG_DB)
-
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'mtg-magical',
-      user:     'test',
-      password: 'pass'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
+      filename: './db/mtgMagical.sqlite3',
     },
     useNullAsDefault: true,
-  },
-  production: {
-    client: 'pg',
-    connection: productionDbConnection, 
     migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './db/seeds',
     },
-    useNullAsDefault: true
   },
 };
+
