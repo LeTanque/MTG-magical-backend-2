3f1fed61063c26628dfecfe848ef17e53cd6ade5
Frank Martinez <frankie.martinez@gmail.com>
2019-07-30 18:41:07 -0700

    added production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,31 @@
 
+const databaseUrl = process.env.DATABASE_URL;
+
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './db/mtgMagical.sqlite3',
+      filename: './data/mtgMagical.sqlite3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './db/migrations',
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './db/seeds',
+      directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: databaseUrl,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  }
 };
 
